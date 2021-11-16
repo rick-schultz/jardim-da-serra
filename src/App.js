d1db638ee61ba360ae38833f5c05a3e17d1f9567
Rick Schultz <rick-schultz@hotmail.com>
2021-11-16 20:39:46 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import FamilyImg from './assets/family.png'
+import Garden from './assets/garden-logo.png'
+import Solutions from './assets/solutions-logo.png'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <main className="main">
+          <div className="header">
+            <h1>iTUDE</h1>
+          </div>
+          <div className="container">
+            <div className="sidebar">
+              <img src={Garden} alt="" />
+              <div className="line"></div>
+              <h1>Poucas Unidades</h1>
+              <button>Saiba Mais</button>
+              <img src={Solutions} alt="" />
+            </div>
+            <div className="background">
+              <img src={FamilyImg} alt="" />
+              <h1>Não fique de fora dessa oportunidade</h1>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  )
 }
-
-export default App;
